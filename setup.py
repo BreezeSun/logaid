ea718cbb1fefa2bbbddc3f9fb945b9b58ffd6bd6
BreezeSun <store177447@gmail.com>
2024-10-16 01:51:12 +0800

    update version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 setup(
-      name='logaid',  # 包的名称
-      version='1.2.3',  # 包的版本号
-      author='BreezeSun',  # 作者姓名
+      name='logaid',
+      version='1.2.4',
+      author='BreezeSun',
       description='A log aid for you.',
       packages=find_packages(),
       long_description=open('README.md','r',encoding='utf-8').read(),
