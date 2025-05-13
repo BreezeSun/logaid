3ea3037e18e46b3055014386e6119c48f0599151
BreezeSun <store177447@gmail.com>
2025-05-14 01:34:42 +0800

    save filename add %M_%S

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
       name='logaid',
-      version='1.3.0',
+      version='1.3.1',
       author='BreezeSun',
       description='A log aid for you.',
       packages=find_packages(),
